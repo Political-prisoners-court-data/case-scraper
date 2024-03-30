0339ef16ff09f30196a0e7ad7e8f432f86edb815
Ivan <fizmat.r66@gmail.com>
2024-03-30 13:22:21 +0400

    use single quotes

--- demhack8/spiders/rfm.py
+++ demhack8/spiders/rfm.py
@@ -8,10 +8,10 @@ from demhack8.items import RfmPersonItem
 
 
 class RfmSpider(Spider):
-    name = "rfm"
-    allowed_domains = ["www.fedsfm.ru"]
-    start_urls = ["https://www.fedsfm.ru/documents/terrorists-catalog-portal-act"]
+    name = 'rfm'
+    allowed_domains = ['www.fedsfm.ru']
+    start_urls = ['https://www.fedsfm.ru/documents/terrorists-catalog-portal-act']
 
     def parse(self, response: Response, **kwargs: Any) -> Iterable[RfmPersonItem]:
-        for person in response.css("#russianFL .terrorist-list li::text"):
+        for person in response.css('#russianFL .terrorist-list li::text'):
             yield RfmPersonItem(person.get())
