23b6e9414e0940d92ab357f733f98a63a80187f4
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2024-07-20 11:30:16 +0300

    Scraping initiation script: returning to the previous bash -c style command

--- scrape_rfm.sh
+++ scrape_rfm.sh
@@ -1,3 +1,3 @@
-bash -c 'http_proxy=http://localhost:4321 https_proxy=http://localhost:4321' &&
-mamba run -n case-scraper scrapy crawl rfm &&
+bash -c 'http_proxy=http://localhost:4321 https_proxy=http://localhost:4321 \
+mamba run -n case-scraper scrapy crawl rfm' &&
 mamba run -n case-scraper ./ingest_rfm.py
