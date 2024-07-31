aa52964f9dd4244f7bd74a4b6a80ede18b0135b6
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2024-07-31 21:34:13 +0100

    Adjustments of PersonEventGenerator: use connection, hide None fields, proper instance variable names

--- generate_events.py
+++ generate_events.py
@@ -10,44 +10,44 @@ from demhack8.items import RfmPersonItem
 
 class PersonEventGenerator:
 
-    def __init__(self, sql_engine, mongo_client):
-        self._sql_engine = sql_engine
-        self._mongo_client = mongo_client
+    def __init__(self, connection, mongo_client):
+        self.__connection = connection
+        self.__mongo_client = mongo_client
 
     def generate(self):
-        with self._sql_engine.begin() as connection:
-            added_result = connection.execute(text('SELECT * FROM rfm_added'))
-            changed_result = connection.execute(text('SELECT * FROM rfm_changed'))
-            removed_result = connection.execute(text('SELECT * FROM rfm_removed'))
-            connection.commit()
+        added_result = self.__connection.execute(text('SELECT * FROM rfm_added'))
+        changed_result = self.__connection.execute(text('SELECT * FROM rfm_changed'))
+        removed_result = self.__connection.execute(text('SELECT * FROM rfm_removed'))
 
         field_names = [self.__to_camel_case(field.name) for field in fields(RfmPersonItem)]
         result_list = []
 
         for changed_row_mapping in changed_result.mappings():
-            changed_event = {self.__to_camel_case(key): val for (key, val) in changed_row_mapping.items() if val}
+            changed_event = {self.__to_camel_case(key): val
+                             for (key, val) in changed_row_mapping.items()
+                             if val is not None}
             self.__fullfill_common_fields(changed_event, 'changed')
             result_list.append(changed_event)
 
-        for row_vals in added_result:
-            add_event = dict(zip(field_names, tuple(row_vals._tuple())))
-            add_event = {k: v for (k, v) in add_event.items() if v}
+        for row_vals in added_result.tuples():
+            add_event = dict(zip(field_names, row_vals))
+            add_event = {k: v for (k, v) in add_event.items() if v is not None}
             self.__fullfill_common_fields(add_event, 'added')
             result_list.append(add_event)
 
-        for row_vals in removed_result:
-            removed_event = dict(zip(field_names, tuple(row_vals._tuple())))
-            removed_event = {k: v for (k, v) in removed_event.items() if v}
+        for row_vals in removed_result.tuples():
+            removed_event = dict(zip(field_names, row_vals))
+            removed_event = {k: v for (k, v) in removed_event.items() if v is not None}
             self.__fullfill_common_fields(removed_event, 'removed')
             result_list.append(removed_event)
 
-        db = self._mongo_client['events']
+        db = self.__mongo_client['events']
         collection = db['events']
 
         if result_list:
             collection.insert_many(result_list, ordered=False)
 
-        self._mongo_client.close()
+        self.__mongo_client.close()
 
     def __fullfill_common_fields(self, event: dict, action: str):
         if event['birthDate']:
@@ -63,5 +63,7 @@ class PersonEventGenerator:
 
 if __name__ == '__main__':
     engine = create_engine('postgresql://db@localhost:5432/postgres')
-    gen = PersonEventGenerator(engine, MongoClient())
-    gen.generate()
+    with engine.begin() as connection:
+        gen = PersonEventGenerator(connection, MongoClient())
+        gen.generate()
+        connection.commit()
